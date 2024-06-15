761d682b175a448ca5f0ea0afe40cd248bcb77f5
Daniel Levi <daniellevi000@gmaiil.com>
2024-06-15 13:32:38 -0300

    feat: create integrations back end login

---
+++ src/api/sign-in.ts
@@ -0,0 +1,7 @@
+import { api } from '@/lib/axios'
+export interface SigInBody {
+  email: string
+}
+export async function signIn({ email }: SigInBody) {
+  await api.post('/authenticate', { email })
+}
