4d646c1e0c9742906fa5bd56d6fc16e347aae0b2
Daniel Levi <daniellevi000@gmaiil.com>
2024-06-18 08:51:32 -0300

    feat: create status actions complete

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -1,9 +1,9 @@
-type OrderStatus =
+export type OrderStatus =
   | 'pending'
+  | 'canceled'
   | 'processing'
   | 'delivering'
   | 'delivered'
-  | 'canceled'
 
 interface OrderStatusProps {
   status: OrderStatus
@@ -11,26 +11,43 @@ interface OrderStatusProps {
 
 const orderStatusMap: Record<OrderStatus, string> = {
   pending: 'Pendente',
-  processing: 'Processando',
-  delivering: 'Entregando',
-  delivered: 'Entregue',
   canceled: 'Cancelado',
+  delivered: 'Entregue',
+  delivering: 'Em entrega',
+  processing: 'Em preparo',
 }
+
 export function OrderStatus({ status }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       {status === 'pending' && (
-        <span className="size-2 rounded-full bg-slate-400" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-slate-400"
+        />
       )}
+
       {status === 'canceled' && (
-        <span className="size-2 rounded-full bg-rose-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-rose-500"
+        />
       )}
+
       {status === 'delivered' && (
-        <span className="size-2 rounded-full bg-emerald-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-emerald-500"
+        />
       )}
-      {['processing', 'delivered'].includes(status) && (
-        <span className="-400 size-2 rounded-full bg-amber-500" />
+
+      {['processing', 'delivering'].includes(status) && (
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-amber-500"
+        />
       )}
+
       <span className="font-medium text-muted-foreground">
         {orderStatusMap[status]}
       </span>
